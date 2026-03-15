8add5875cea94dbfc8574a1c9a7c615c8fad1828
Abishake Raman <144309921+abi9502@users.noreply.github.com>
2026-03-15 20:27:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages-ல் Blank Page வராமல் இருக்க இந்த Base URL முக்கியம்:
+  base: './',
 })
